d24dcfb0b231ab800e5294c3d0e0ae6ca7c87703
Asma Mohammed <asmaelsayeh@tusomi.onmicrosoft.com>
2024-10-03 18:03:51 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ain_shams1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
